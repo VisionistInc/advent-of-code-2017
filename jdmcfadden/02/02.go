1c4678267bc83b806570bf05219e44ac37238772
Josh McFadden <mindsound@gmail.com>
2017-12-02 00:42:26 -0500

    Day 2 solutions.

---
+++ jdmcfadden/02/02.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func main() {
+	inputfile, _ := os.Open("./input")
+	csvreader := csv.NewReader(inputfile)
+	csvreader.Comma = '\t'
+	sheet, _ := csvreader.ReadAll()
+
+	// parse all the strings
+	var numbers [][]float64
+	for _, row := range sheet {
+		var parsedrow []float64
+		for _, cell := range row {
+			value, _ := strconv.ParseFloat(cell, 64)
+			parsedrow = append(parsedrow, value)
+		}
+		numbers = append(numbers, parsedrow)
+	}
+
+	var cksum float64
+	for _, row := range numbers {
+		min := math.MaxFloat64
+		max := -min
+		for _, number := range row {
+			min = math.Min(min, number)
+			max = math.Max(max, number)
+		}
+		diff := max - min
+		cksum += diff
+	}
+	fmt.Println(cksum)
+
+	// for each number in a sorted row, divide the sorted numbers by that number
+	// and bail on fmod 1.0 = 0.0
+	var divsum float64
+	for _, row := range numbers {
+		sort.Float64s(row)
+		var n1, n2 float64
+		for ii, number := range row {
+			for _, greaternumber := range row[ii+1:] {
+				if math.Mod(greaternumber/number, 1.0) == 0.0 {
+					n1 = number
+					n2 = greaternumber
+					divsum += (n2 / n1)
+				}
+			}
+		}
+	}
+	fmt.Println(divsum)
+
+}
