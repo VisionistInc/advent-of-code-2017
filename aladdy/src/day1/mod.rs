30b1c2a6f8bc286b99c970c92230b9e0a34fe81c
Alex Addy <alex.addy@gmail.com>
2017-12-02 20:45:34 -0500

    Add solution for day one part two.

--- aladdy/src/day1/mod.rs
+++ aladdy/src/day1/mod.rs
@@ -4,17 +4,24 @@ pub fn solve<L: Iterator<Item=Result<String>>>(input: &mut L) -> () {
     let line = input.next().unwrap().unwrap();
     let bytes = line.into_bytes();
 
-    let mut sum: u64 = 0;
     if bytes.len() < 2 {
-        println!("The captcha is {}", sum);
+        println!("The input is too short.");
         return;
     }
 
+    let mut sum1: u64 = 0;
+    let mut sum2: u64 = 0;
+
+    let len = bytes.len();
     for i in 0..bytes.len() {
-        if bytes[i] == bytes[(i+1)%bytes.len()] {
-            sum += (bytes[i] as u64)-48;
+        if bytes[i] == bytes[(i+1)%len] {
+            sum1 += (bytes[i] as u64)-48; // 48 == '0'
+        }
+        if bytes[i] == bytes[(i+len/2)%len] {
+            sum2 += (bytes[i] as u64)-48; // 48 == '0'
         }
     }
 
-    println!("The captcha is {}", sum);
+    println!("The captcha is {}", sum1);
+    println!("The secondary captcha is {}", sum2);
 }
