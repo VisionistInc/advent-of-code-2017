243f839081bc23d5611ef413adcfd240728bb451
Dillon Mulroy <dillon.mulroy@visionistinc.com>
2017-12-04 14:10:18 -0500

    Finish day 3 part 1

--- dmmulroy/03/part1.js
+++ dmmulroy/03/part1.js
@@ -1,8 +1,46 @@
 const input = 361527;
 
-const nearestPerfectSquare = Math.round(Math.sqrt(input));
+var calcDistance = input => {
+  const nearestPerfectSquareRoot = Math.round(Math.sqrt(input));
 
-const nearestPerfectSquareCoordinates =
-  nearestPerfectSquare % 2 === 0
-    ? [-(nearestPerfectSquare / 2 - 1), nearestPerfectSquare / 2]
-    : [(nearestPerfectSquare - 1) / 2, -(nearestPerfectSquare - 1) / 2];
+  const nearestPerfectSquare =
+    nearestPerfectSquareRoot * nearestPerfectSquareRoot;
+
+  const isEven = nearestPerfectSquareRoot % 2 === 0;
+
+  const roundedUp = Math.sqrt(input) % nearestPerfectSquareRoot > 0.5;
+
+  const nearestPerfectSquareRootCoordinates = isEven
+    ? [-(nearestPerfectSquareRoot / 2 - 1), nearestPerfectSquareRoot / 2]
+    : [(nearestPerfectSquareRoot - 1) / 2, -(nearestPerfectSquareRoot - 1) / 2];
+
+  let [x, y] = nearestPerfectSquareRootCoordinates;
+
+  if (!isEven && !roundedUp) {
+    x++;
+    for (let i = nearestPerfectSquare + 1; i < input; i++) {
+      if (i === input) break;
+      y++;
+    }
+  } else if (!isEven && roundedUp) {
+    for (let i = nearestPerfectSquare; i > input; i--) {
+      if (i === input) break;
+      x--;
+    }
+  } else if (isEven && !roundedUp) {
+    x--;
+    for (let i = nearestPerfectSquare + 1; i < input; i++) {
+      if (i === input) break;
+      y--;
+    }
+  } else {
+    for (let i = nearestPerfectSquare; i > input; i--) {
+      if (i === input) break;
+      x++;
+    }
+  }
+
+  return Math.abs(x) + Math.abs(y);
+};
+
+console.log('output:', calcDistance(input));
