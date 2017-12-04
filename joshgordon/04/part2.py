e64a74c27d936b5ae0afb40460bfb3d2a6b89e83
Josh Gordon <github@joshgordon.net>
2017-12-04 18:15:48 -0500

    joshgordon day4.

---
+++ joshgordon/04/part2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+if len(sys.argv) > 1:
+  infilename = sys.argv[1]
+else:
+  infilename = 'input.txt'
+
+passwords = []
+
+with open(infilename, 'r') as infile:
+  for line in infile:
+    passwords.append(line.strip().split(' '))
+
+count = 0
+for password in passwords:
+  password = [''.join(sorted(list(word))) for word in password]
+  if len(password) == len(set(password)):
+    count += 1
+
+print(count)
+
