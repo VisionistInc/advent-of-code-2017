34bae140df8e6b6c6cf1eb350f54ee6df6b56781
Dillon Mulroy <dillon.mulroy@gmail.com>
2017-12-10 12:04:32 -0500

    Day 8 part 2

---
+++ dmmulroy/08/part2.js
@@ -0,0 +1,80 @@
+const fs = require('fs');
+const path = require('path');
+
+const input = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8');
+
+const evaluateCondition = (
+  comparisonOpertator,
+  comparisonRegister,
+  comparisonValue
+) => {
+  switch (comparisonOpertator) {
+    case '>':
+      return Number(comparisonRegister) > Number(comparisonValue);
+    case '<':
+      return Number(comparisonRegister) < Number(comparisonValue);
+    case '>=':
+      return Number(comparisonRegister) >= Number(comparisonValue);
+    case '<=':
+      return Number(comparisonRegister) <= Number(comparisonValue);
+    case '!=':
+      return Number(comparisonRegister) !== Number(comparisonValue);
+    case '==':
+      return Number(comparisonRegister) === Number(comparisonValue);
+    default:
+      return false;
+  }
+};
+
+const findMaxValue = input => Math.max(...Object.values(input));
+
+const sanitizedInput = input
+  .trim()
+  .split('\n')
+  .reduce(
+    (prev, curr, idx, arr) => {
+      const [
+        register,
+        action,
+        amount,
+        _,
+        comparisonRegister,
+        comparisonOpertator,
+        comparisonValue
+      ] = curr.split(' ');
+
+      if (!prev[register]) {
+        prev[register] = 0;
+      }
+
+      if (!prev[comparisonRegister]) {
+        prev[comparisonRegister] = 0;
+      }
+
+      const evaluation = evaluateCondition(
+        comparisonOpertator,
+        prev[comparisonRegister],
+        comparisonValue
+      );
+
+      if (!evaluation && idx !== arr.length - 1) {
+        prev['max'] = Math.max(...Object.values(prev));
+        return prev;
+      }
+
+      if (action === 'inc') {
+        prev[register] += Number(amount);
+      } else {
+        prev[register] -= Number(amount);
+      }
+
+      prev['max'] = Math.max(...Object.values(prev));
+
+      return prev;
+    },
+    { max: 0 }
+  );
+
+const { max: answer } = sanitizedInput;
+
+console.log('answer', answer);
