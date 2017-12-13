16c80824001ad6df34b0a5c09f8279dd13906834
Dillon Mulroy <dillon.mulroy@visionistinc.com>
2017-12-13 09:33:38 -0500

    Day 10 part 2

--- dmmulroy/10/part2.js
+++ dmmulroy/10/part2.js
@@ -1,17 +1,21 @@
-const lengths = require('./input');
+const lengths = '120,93,0,90,5,80,129,74,1,165,204,255,254,2,50,113';
 
 const createList = () => {
   let list = [];
   for (let idx = 0; idx < 256; idx++) {
-    list[idx] = idx.toString().charCodeAt();
+    list[idx] = idx;
   }
-  return list.concat([17, 31, 73, 47, 23]);
+  return list;
 };
+
+const convertToASCII = str => str.split('').map(char => char.charCodeAt());
 
 const knotHash = (list, lengths) => {
   let skip = 0;
   let currentPos = 0;
   let denseHash = [];
+
+  lengths.push(...[17, 31, 73, 47, 23]);
 
   for (let rounds = 0; rounds < 64; rounds++) {
     lengths.forEach(length => {
@@ -25,18 +29,26 @@ const knotHash = (list, lengths) => {
         list[(currentPos + idx) % list.length] =
           sliceToReverse[length - idx - 1];
       }
+
       currentPos += (length + skip) % list.length;
       skip++;
     });
   }
 
   for (let block = 0; block < 16; block++) {
+    let XOR = 0;
     for (let blockValue = 0; blockValue < 16; blockValue++) {
-      denseHash[block] = denseHash[block] || 0 ^ list[block * 16 + blockValue];
+      XOR = XOR ^ list[block * 16 + blockValue];
     }
+    denseHash[block] = XOR;
   }
 
-  return denseHash.map(byte => ('0' + (byte & 0xff)).toString(16)).join('');
+  return denseHash
+    .map(byte => {
+      const hex = byte.toString(16);
+      return hex.length === 2 ? hex : `0${hex}`;
+    })
+    .join('');
 };
 
-console.log(knotHash(createList(), lengths));
+console.log(knotHash(createList(), convertToASCII(lengths)));
