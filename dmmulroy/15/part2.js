35a05bd35d089dedcc4984c5ae93cbb07ab1c013
Dillon Mulroy <dillon.mulroy@visionistinc.com>
2017-12-15 15:49:25 -0500

    Day 15 part 2

--- dmmulroy/15/part2.js
+++ dmmulroy/15/part2.js
@@ -7,19 +7,29 @@ const judge = (a, b) => a.slice(-16) === b.slice(-16);
 const zeropad = str =>
   str.length >= 16 ? str : `${new Array(17 - str.length).join('0')}${str}`;
 
-const toBinary = str => parseInt(str, 10).toString(2);
+const toBinary = num => (num >>> 0).toString(2);
 
 const calculatePairs = (a = 289, b = 629) => {
   let pairs = 0;
-  for (let iter = 0; iter < 40000000; iter++) {
+
+  for (let iter = 0; iter < 5000000; iter++) {
     a = (a * aFactor) % divisor;
     b = (b * bFactor) % divisor;
 
-    if (judge(zeropad(toBinary(a)), zeropad(toBinary(b)))) pairs++;
+    while (a % 4 !== 0) {
+      a = (a * aFactor) % divisor;
+    }
+
+    while (b % 8 !== 0) {
+      b = (b * bFactor) % divisor;
+    }
+
+    if (judge(zeropad(toBinary(a)), zeropad(toBinary(b)))) {
+      pairs++;
+    }
   }
+
   return pairs;
 };
 
-// console.time('time');
 console.log('output:', calculatePairs());
-// console.timeEnd('time');
