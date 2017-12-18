3c2c989a5e767a7d69f2f4056591dd0d5cd13fc0
Chris Cavey <cicavey+aoc@visionistinc.com>
2017-12-18 15:39:46 -0500

    Day 18 part 1

---
+++ cicavey/18/rust/src/main.rs
@@ -0,0 +1,173 @@
+use std::io::BufReader;
+use std::fs::File;
+use std::io::prelude::*;
+use std::str;
+use std::collections::HashMap;
+
+#[derive(Debug)]
+struct Program {
+    pub regs: HashMap<String, i64>,
+    pub pc: usize,
+    pub ic: u64,
+}
+
+impl Program {
+    pub fn new() -> Self {
+        Program {
+            regs: HashMap::with_capacity(27),
+            pc: 0,
+            ic: 0,
+        }
+    }
+    pub fn new_id(id: i64) -> Self {
+        let mut p = Self::new();
+        p.regs.insert("p".to_string(), id);
+        return p;
+    }
+
+    pub fn lookup(&mut self, reg_or_value: &str) -> i64 {
+        match reg_or_value.parse() {
+            Ok(n) => n,
+            Err(_) => { self.regs.entry(reg_or_value.to_string()).or_insert(0).clone() },
+        }
+    }
+}
+
+fn main() {
+    let file = File::open("../input.txt").unwrap();
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents).unwrap();
+
+    let mut p = Program::new();
+    let mut first_rcv = true;
+    let code: Vec<String> = contents.lines().map(|s| s.to_string()).collect();
+    while p.pc < code.len() && p.ic < 10000 {
+        //println!("IC: {:09} PC: {:04} {}", p.ic, p.pc, code[p.pc]);
+        let next_instruction = &code[p.pc];
+        let sins: Vec<String> = next_instruction.split_whitespace().map(|s| s.to_string()).collect();
+        let mut step_pc = true;
+        match sins[0].as_str() {
+            "snd" => {
+                let v = p.lookup(&sins[1]);
+                p.regs.insert("snd".to_string(), v);
+            },
+            "set" => {
+                let v = p.lookup(&sins[2]);
+                p.regs.insert(sins[1].to_string(), v);
+            },
+            "add" => {
+                let v = p.lookup(&sins[2]);
+                let t = p.lookup(&sins[1]);
+                p.regs.insert(sins[1].to_string(), v+t);
+            },
+            "mul" => {
+                let v = p.lookup(&sins[2]);
+                let t = p.lookup(&sins[1]);
+                p.regs.insert(sins[1].to_string(), v*t);
+            },
+            "mod" => {
+                let v = p.lookup(&sins[2]);
+                let t = p.lookup(&sins[1]);
+                p.regs.insert(sins[1].to_string(), t % v);
+            },
+            "rcv" => {
+                let v = p.lookup(&sins[1]);
+                if v != 0 {
+                    let s = p.lookup("snd");
+                    p.regs.insert("rcv".to_string(), s);
+                    if first_rcv {
+                        println!("FIRST_RCV: {}", s);
+                        first_rcv = false;
+                        p.pc = code.len();
+                    }
+                }
+            },
+            "jgz" => {
+                let offset = p.lookup(&sins[2]);
+                let test = p.lookup(&sins[1]);
+                if test > 0 {
+                    step_pc = false;
+                    p.pc = (p.pc as i64 + offset) as usize;
+                }
+            },
+            _ => (),
+        };
+
+        if step_pc {
+            p.pc += 1;
+        }
+
+        p.ic += 1;
+
+        //println!("\tPC:{} {:?}", p.pc, p.regs);
+    }
+
+    let mut limit = 0;
+    let mut programs = [Program::new_id(0), Program::new_id(1)];
+    loop {
+        for pidx in 0..2 {
+            let next_instruction = &code[programs[pidx].pc];
+            let sins: Vec<String> = next_instruction.split_whitespace().map(|s| s.to_string()).collect();
+            let mut step_pc = true;
+            match sins[0].as_str() {
+                "snd" => {
+                    let v = programs[pidx].lookup(&sins[1]);
+                    programs[pidx].regs.insert("snd".to_string(), v);
+                },
+                "set" => {
+                    let v = programs[pidx].lookup(&sins[2]);
+                    programs[pidx].regs.insert(sins[1].to_string(), v);
+                },
+                "add" => {
+                    let v = programs[pidx].lookup(&sins[2]);
+                    let t = programs[pidx].lookup(&sins[1]);
+                    programs[pidx].regs.insert(sins[1].to_string(), v+t);
+                },
+                "mul" => {
+                    let v = programs[pidx].lookup(&sins[2]);
+                    let t = programs[pidx].lookup(&sins[1]);
+                    programs[pidx].regs.insert(sins[1].to_string(), v*t);
+                },
+                "mod" => {
+                    let v = programs[pidx].lookup(&sins[2]);
+                    let t = programs[pidx].lookup(&sins[1]);
+                    programs[pidx].regs.insert(sins[1].to_string(), t % v);
+                },
+                "rcv" => {
+                    let v = programs[pidx].lookup(&sins[1]);
+                    if v != 0 {
+                        let s = programs[pidx].lookup("snd");
+                        programs[pidx].regs.insert("rcv".to_string(), s);
+                        if first_rcv {
+                            println!("FIRST_RCV: {}", s);
+                            first_rcv = false;
+                            programs[pidx].pc = code.len();
+                        }
+                    }
+                },
+                "jgz" => {
+                    let offset = programs[pidx].lookup(&sins[2]);
+                    let test = programs[pidx].lookup(&sins[1]);
+                    if test > 0 {
+                        step_pc = false;
+                        programs[pidx].pc = (programs[pidx].pc as i64 + offset) as usize;
+                    }
+                },
+                _ => (),
+            };
+
+            if step_pc {
+                programs[pidx].pc += 1;
+            }
+
+            programs[pidx].ic += 1;
+        }
+
+
+        limit += 1;
+        if limit > 1000 {
+            break;
+        }
+    }
+}
