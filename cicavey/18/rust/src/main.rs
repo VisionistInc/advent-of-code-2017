d38da84a649334194043f80262c639f6b3ad3301
Chris Cavey <cicavey+aoc@visionistinc.com>
2017-12-19 13:12:28 -0500

    Day 18 Part 2

--- cicavey/18/rust/src/main.rs
+++ cicavey/18/rust/src/main.rs
@@ -3,12 +3,16 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::str;
 use std::collections::HashMap;
+use std::collections::VecDeque;
 
 #[derive(Debug)]
 struct Program {
-    pub regs: HashMap<String, i64>,
-    pub pc: usize,
-    pub ic: u64,
+    regs: HashMap<String, i64>,
+    pc: usize,
+    ic: u64,
+    send_buffer: VecDeque<i64>,
+    send_count: u64,
+    blocked: bool,
 }
 
 impl Program {
@@ -17,6 +21,9 @@ impl Program {
             regs: HashMap::with_capacity(27),
             pc: 0,
             ic: 0,
+            send_buffer: VecDeque::new(),
+            send_count: 0,
+            blocked: false
         }
     }
     pub fn new_id(id: i64) -> Self {
@@ -38,10 +45,12 @@ fn main() {
     let mut buf_reader = BufReader::new(file);
     let mut contents = String::new();
     buf_reader.read_to_string(&mut contents).unwrap();
+    
+    let code: Vec<String> = contents.lines().map(|s| s.to_string()).collect();
 
+    /*
     let mut p = Program::new();
     let mut first_rcv = true;
-    let code: Vec<String> = contents.lines().map(|s| s.to_string()).collect();
     while p.pc < code.len() && p.ic < 10000 {
         //println!("IC: {:09} PC: {:04} {}", p.ic, p.pc, code[p.pc]);
         let next_instruction = &code[p.pc];
@@ -102,10 +111,12 @@ fn main() {
 
         //println!("\tPC:{} {:?}", p.pc, p.regs);
     }
+    */
 
     let mut limit = 0;
     let mut programs = [Program::new_id(0), Program::new_id(1)];
     loop {
+        // alternate between the two programs
         for pidx in 0..2 {
             let next_instruction = &code[programs[pidx].pc];
             let sins: Vec<String> = next_instruction.split_whitespace().map(|s| s.to_string()).collect();
@@ -113,7 +124,10 @@ fn main() {
             match sins[0].as_str() {
                 "snd" => {
                     let v = programs[pidx].lookup(&sins[1]);
-                    programs[pidx].regs.insert("snd".to_string(), v);
+                    //programs[pidx].regs.insert("snd".to_string(), v);
+                    programs[(pidx + 1) % 2].send_buffer.push_back(v);
+                    //println!("send: {} +1 {}", pidx, programs[pidx].send_count);
+                    programs[pidx].send_count += 1;
                 },
                 "set" => {
                     let v = programs[pidx].lookup(&sins[2]);
@@ -135,15 +149,13 @@ fn main() {
                     programs[pidx].regs.insert(sins[1].to_string(), t % v);
                 },
                 "rcv" => {
-                    let v = programs[pidx].lookup(&sins[1]);
-                    if v != 0 {
-                        let s = programs[pidx].lookup("snd");
-                        programs[pidx].regs.insert("rcv".to_string(), s);
-                        if first_rcv {
-                            println!("FIRST_RCV: {}", s);
-                            first_rcv = false;
-                            programs[pidx].pc = code.len();
-                        }
+                    step_pc = false;
+                    if let Some(v) = programs[pidx].send_buffer.pop_front() {
+                        step_pc = true;
+                        programs[pidx].regs.insert(sins[1].to_string(), v);
+                        programs[pidx].blocked = false;
+                    } else {
+                        programs[pidx].blocked = true;
                     }
                 },
                 "jgz" => {
@@ -164,9 +176,16 @@ fn main() {
             programs[pidx].ic += 1;
         }
 
+        if programs[0].blocked && programs[1].blocked {
+            println!("{} deadlock: {} {}", limit, programs[0].send_count, programs[1].send_count);
+            println!("0> IC:{:09} PC:{:04} R:{:?} SB:{:?}", programs[0].ic, programs[0].pc, programs[0].regs, programs[0].send_buffer);
+            println!("1> IC:{:09} PC:{:04} R:{:?} SB:{:?}", programs[1].ic, programs[1].pc, programs[1].regs, programs[1].send_buffer);
+            break;
+        }
 
         limit += 1;
-        if limit > 1000 {
+        if limit > 1000000 {
+            println!("limit reached");
             break;
         }
     }
